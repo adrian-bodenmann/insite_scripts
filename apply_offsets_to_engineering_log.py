20e7edeeb1e493079de32881259720391eba54fc
Adrian Bodenmann <adrian.bodenmann@soton.ac.uk>
2023-06-16 20:33:04 +0100

    Add script to apply temporal and orientational offsets to ALR nav data

---
+++ apply_offsets_to_engineering_log.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import argparse
+
+
+def main(input_path, output_path, offset_s, offset_roll_rad, offset_pitch_rad):
+    df = pd.read_csv(input_path)
+    df["corrected_timestamp"] += offset_s
+    df["AUV_roll"] += offset_roll_rad
+    df["AUV_pitch"] += offset_pitch_rad
+    df.to_csv(output_path, index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_path", type=str)
+    parser.add_argument("output_path", type=str)
+    parser.add_argument("offset_s", type=float)
+    parser.add_argument("offset_roll_rad", type=float)
+    parser.add_argument("offset_pitch_rad", type=float)
+    args = parser.parse_args()
+    main(args.input_path, args.output_path, args.offset_s, args.offset_roll_rad, args.offset_pitch_rad)
+
+
+# Example usage:
+'''
+python ./apply_offsets_to_engineering_log.py D:/cruises/2022/insite/comparison_attitude_BioCam-ALR/20220923_130737_alr_bc4k15c_mapping/corrected_engineering_log.csv D:/cruises/2022/insite/comparison_attitude_BioCam-ALR/20220923_130737_alr_bc4k15c_mapping/engineering_log_corrected_2.csv -1.6 0.033161 -0.008727
+python ./apply_offsets_to_engineering_log.py D:/cruises/2022/insite/comparison_attitude_BioCam-ALR/20221004_153010_alr_bc4k15c_mapping/engineering_log_corrected.csv D:/cruises/2022/insite/comparison_attitude_BioCam-ALR/20221004_153010_alr_bc4k15c_mapping/engineering_log_corrected_2.csv -1.6 0.033161 -0.008727
+python ./apply_offsets_to_engineering_log.py D:/cruises/2022/insite/comparison_attitude_BioCam-ALR/20221006_095110_alr_bc4k15c_mapping/engineering_log_corrected.csv D:/cruises/2022/insite/comparison_attitude_BioCam-ALR/20221006_095110_alr_bc4k15c_mapping/engineering_log_corrected_2.csv -1.6 0.033161 -0.008727
+'''
