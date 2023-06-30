d7665ecf7709b2ed756304ccf0463178a7e6b57e
Adrian Bodenmann <adrian.bodenmann@soton.ac.uk>
2023-06-30 11:47:00 +0100

    Add axis labels

--- plot_methane_radings.py
+++ plot_methane_radings.py
@@ -3,4 +3,6 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("D:/raw/2022/insite/alr/M77 to M82/franatech_mets_time_corrected_20220923.csv") 
 df["timestamp"] = pd.to_datetime(df["corrected_timestamp"], unit='s')
 plt.plot(df["timestamp"], df["methane concentration(umol/l)"])
+plt.xlabel("Time")
+plt.ylabel("Methane concentration (umol/l)")
 plt.show()
